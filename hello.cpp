e454adbd1a40c88ca88e56af77778917362ae04d
jedmeyer <jedmeyer@stolaf.edu>
2019-05-18 01:13:19 -0400

    whoops.

--- hello.cpp
+++ hello.cpp
@@ -67,6 +67,7 @@ void my_hello_world::main(std::string /*url*/)
         qmap.insert(pair<string, string>("zdumb", "zdumb"));
     for(int i=0; i<qgive.size(); i++)
         cout << qgive[i];
+        
     
 
     // ------------------LANDING Page-----------------------//
