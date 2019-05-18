ac1f5239bdff9ae46d25a427c13260ff3ee28dd0
jedmeyer <jedmeyer@stolaf.edu>
2019-05-17 21:26:15 -0400

    Cleaning up includes.

--- hello.cpp
+++ hello.cpp
@@ -7,9 +7,9 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
-#include<vector>
-#include"helper_functions.hpp"
-#include"FAclassEpsilon.cpp"
+#include <vector>
+#include "helper_functions.hpp"
+#include "FAclassEpsilon.cpp"
 using namespace std;
 
 
